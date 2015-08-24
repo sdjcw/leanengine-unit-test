bd0309f1fc2da8f92987f93f6965737642e90d79
wchen <sdjcw13@gmail.com>
2015-08-24 16:15:59 +0800

    这是一个无法编译的提交，为了测试应用启动失败的处理

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ var APP_KEY = process.env.LC_APP_KEY;
 var MASTER_KEY = process.env.LC_APP_MASTER_KEY;
 
 AV.initialize(APP_ID, APP_KEY, MASTER_KEY);
-require('coffee-script/register')
+AV.Cloud.useMasterKey();
+require('coffee-script/register'
 var app = require('./app');
 
 // 端口一定要从环境变量 `LC_APP_PORT` 中获取。
